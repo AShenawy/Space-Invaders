4f8496b049149fb20338b8135b6fd2648a27d770
jsapoval <90786236+jsapoval@users.noreply.github.com>
2021-12-22 01:36:07 +0200

    my branch
    here i changed graphics and sounds, i tried to add ui as well, but i gave up in the end cause order of the scenes didn't work :(

--- Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // This script controls the behaviour of each single Alien enemy
 public class EnemyBehaviour : MonoBehaviour
@@ -8,31 +7,38 @@ public class EnemyBehaviour : MonoBehaviour
     public AudioSource audio;
     public AudioClip destroySFX;
 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-	// A function automatically triggerred when another game object with Collider2D component
-	// Enters the Collider2D boundaries on this game object
+
+
+
+    // A function automatically triggerred when another game object with Collider2D component
+    // Enters the Collider2D boundaries on this game object
     private void OnTriggerEnter2D(Collider2D otherCollider)
     {
-		// Check the tag on the other game object. If it's the projectile's tag,
-		//  destroy both this game object and the projectile
+        // Check the tag on the other game object. If it's the projectile's tag,
+        //  destroy both this game object and the projectile
         if (otherCollider.tag == "Projectile")
         {
+
             audio.PlayOneShot(destroySFX);
             Destroy(gameObject);
-			
-			// Get the game object, as a whole, that's attached to the Collider2D component
+
+            // Get the game object, as a whole, that's attached to the Collider2D component
             Destroy(otherCollider.gameObject);
+
         }
     }
+
 }
